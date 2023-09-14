fc0be661e846451669b3efad8cd4fba2b9ae9b8c
Gabriel Aragão Correia de Araújo <97701396+gabrielaragao01@users.noreply.github.com>
2023-09-14 19:25:22 -0300

    Add files via upload

---
+++ Hanoi.asm
@@ -0,0 +1,17 @@
+.386
+.model flat, stdcall
+option casemap :none
+
+include \masm32\include\kernel32.inc
+include \masm32\include\masm32.inc
+includelib \masm32\lib\kernel32.lib
+includelib \masm32\lib\masm32.lib
+
+.data
+    message db "Secso!!!", 0
+
+.code 
+main:
+    invoke StdOut, addr message
+    invoke ExitProcess, 0
+end main
